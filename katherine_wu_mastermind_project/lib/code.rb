c808f2d1db147b8cf8d3ab014760fa06366e7d41
kw-8 <kwu11235813@gmail.com>
2021-08-18 07:41:58 -0700

    all code class except near guess

--- katherine_wu_mastermind_project/lib/code.rb
+++ katherine_wu_mastermind_project/lib/code.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 class Code
   POSSIBLE_PEGS = {
     "R" => :red,
@@ -6,4 +8,50 @@ class Code
     "Y" => :yellow
   }
 
+  attr_reader :pegs
+
+  def self.valid_pegs?(arr)
+    arr.all?{|el| POSSIBLE_PEGS.has_key?(el.upcase)}
+  end
+
+  def initialize(peg_arr)
+    if self.class.valid_pegs?(peg_arr)
+      @pegs = peg_arr
+      @pegs.map!{|peg| peg.upcase}
+    else raise "this is not an array of valid pegs"
+    end
+  end
+
+  def self.random(num)
+    arr = (1..num).map {|i| POSSIBLE_PEGS.keys.sample}
+    Code.new(arr)
+  end
+
+  def self.from_string(str)
+    Code.new(str.split(""))
+  end
+
+  def [](index)
+    @pegs[index]
+  end
+
+  def length
+    @pegs.length
+  end
+
+  '''
+  PART 2
+  '''
+
+  def num_exact_matches(code_guess)
+    code_guess.pegs.select.with_index{|ch, i| ch == @pegs[i] }.count
+  end
+
+  def num_near_matches(code_guess)
+    code_guess.pegs.select.with_index{|ch, i| @pegs.include?(ch) && ch != @pegs[i] }.count
+  end
+
+  def  ==(other_code)
+    @pegs == other_code.pegs
+  end
 end
