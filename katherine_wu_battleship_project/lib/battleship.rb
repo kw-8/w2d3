29cb06c84646155a397aa9aaffa3f75b870fe633
kw-8 <kwu11235813@gmail.com>
2021-08-18 12:05:21 -0700

    finished battleship

--- katherine_wu_battleship_project/lib/battleship.rb
+++ katherine_wu_battleship_project/lib/battleship.rb
@@ -2,5 +2,44 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
+    attr_reader :board, :player
 
+    def initialize(n)
+        @player = Player.new
+        @board = Board.new(n)
+        @remaining_misses = @board.size/2
+    end
+
+    def start_game
+        @board.place_random_ships
+        puts @board.size/4
+        @board.print
+    end
+
+    def lose?
+        if @remaining_misses > 0
+            false
+        else puts "you lose"
+            true
+        end
+    end
+
+    def win?
+        if @board.num_ships == 0
+            puts "you win"
+            true
+        else false
+        end
+    end
+
+    def game_over?
+        win? || lose? ? true : false
+    end
+
+    def turn
+        @remaining_misses -= 1 if !@board.attack( @player.get_move )
+        @board.print
+        puts "remaining misses: "
+        p @remaining_misses
+    end
 end
--- katherine_wu_battleship_project/lib/player.rb
+++ katherine_wu_battleship_project/lib/player.rb
@@ -1,3 +1,9 @@
 class Player
+    def get_move
+        puts "enter a position with coordinates separated with a space like `4 7`"
+        pos = []
+        gets.chomp.split(" ").each{|n| pos << n.to_i}
+        pos
+    end
 
 end
