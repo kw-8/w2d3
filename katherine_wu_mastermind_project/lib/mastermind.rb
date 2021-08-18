c51bd272ca5eea3ed5eccf12653ef16dc545fbb0
kw-8 <kwu11235813@gmail.com>
2021-08-18 09:01:31 -0700

    finished mastermind

--- katherine_wu_mastermind_project/lib/mastermind.rb
+++ katherine_wu_mastermind_project/lib/mastermind.rb
@@ -1,5 +1,22 @@
 require_relative "code"
+require "byebug"
 
 class Mastermind
+    def initialize(length)
+        @secret_code = Code.random(length)
+    end
+
+    def print_matches(code_guess)
+        puts "exact matches: " + @secret_code.num_exact_matches(code_guess).to_s
+        puts "near matches: " + @secret_code.num_near_matches(code_guess).to_s
+    end
+
+    def ask_user_for_guess
+        puts "Enter a code"
+        input = gets.chomp
+        guess = Code.from_string(input)
+        print_matches(guess)
+        @secret_code.num_exact_matches(guess) == @secret_code.length
+    end
 
 end
